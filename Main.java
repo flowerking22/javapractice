6ee52af8b121407efbcc67ab0a8e5f9ed3bf85f3
Flower King <100342127+flowerking22@users.noreply.github.com>
2023-01-11 17:35:47 +0530

    Practice java methods vs static

--- Main.java
+++ Main.java
@@ -1,7 +1,30 @@
 import java.util.*;
 
 public class Main {
+ static int x=5;
     public static void main(String[] args) {
-      System.out.println("Hello, World!");
+    System.out.println("-----------Main Code----------");
+   
+    System.out.println(Main.x);
+ hello();
+  Outer.hello();
+ Main main=new Main();
+  System.out.println(main.x);
+ main.hellom();
+ Outer outer=new Outer();
+ outer.hellom();
+
   }
-}
+  static void /*String*/ hello(){
+         System.out.println("Hello,  static , Main");
+         //return "0";
+  }
+   void /*String*/ hellom(){
+         System.out.println("Hello , method , Main");
+         //return "0";
+  }
+}class Outer{
+  protected static void /*String*/ hello(){
+         System.out.println("Hello,  static , Outer");
+         //return "0";
+  }
