614ba3014ebe1495201d736b49aa359d8611bddd
Flower King <100342127+flowerking22@users.noreply.github.com>
2023-01-11 17:54:41 +0530

    Object Creation in java

---
+++ Object.java
@@ -0,0 +1,20 @@
+class IPhone extends Apple {
+  private String model = "iPhone 11";    
+ 
+	public static void main(String args[]) {
+
+    IPhone myMobile = new IPhone();
+
+    System.out.println(myMobile.brand + "'s new realease is " + myMobile.model);
+    myMobile.Features();
+
+  }
+}
+class Apple {
+  protected String brand = "Apple";        
+  public void Features() {                    
+    System.out.println("Capacity : 256 GB");
+    System.out.println("Liquid retina HD Display of 6.1 inches.");
+    System.out.println("Dual 12MP Ultra Wide and Wide cameras");
+  }
+}
