22610ce151d8bb9fe8e836df9025d16cc696ae8a
Flower King <100342127+flowerking22@users.noreply.github.com>
2023-01-13 12:25:50 +0530

    invertedhalf Pyramid with Numbers

---
+++ satr pattern/invertedhalf num.java
@@ -0,0 +1,22 @@
+    static void invertedhalfnumbers(int r){
+  System.out.println("----invertedhalf Pyramid with Numbers-----\n n is "+r);
+  for (int i=r;i>=1;i--){
+    for(int j=1;j<=i;j++){
+    System.out.print(j);
+    }
+    System.out.println();
+  }
+  
+  
+  
+  Output:
+
+----invertedhalf Pyramid with Numbers-----
+ n is 7
+1234567
+123456
+12345
+1234
+123
+12
+1
