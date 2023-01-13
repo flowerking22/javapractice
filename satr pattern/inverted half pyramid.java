b07427477e13c1598fd48f87addf4ece0eed812c
Flower King <100342127+flowerking22@users.noreply.github.com>
2023-01-13 11:36:23 +0530

    Update inverted half pyramid.java

--- satr pattern/inverted half pyramid.java
+++ satr pattern/inverted half pyramid.java
@@ -3,3 +3,33 @@ Print The Pattern:-
      **
     ***
    ****
+
+import java.util.*;
+
+class InvertedPyramid{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    invertedpyramid(r);
+    
+  }
+  public static void invertedpyramid(int r){
+    for (int i=1;i<=r;i++){
+      for(int j=i;j<r;j++){
+        System.out.print(" ");
+      }
+      for(int k=1;k<=i;k++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+
+Output:
+
+   *
+  **
+ ***
+****
