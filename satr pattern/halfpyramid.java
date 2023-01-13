edf15f690d48831a5e08d7a27f822a35b7f05485
Flower King <100342127+flowerking22@users.noreply.github.com>
2023-01-13 11:15:21 +0530

    Create halfpyramid.java

---
+++ satr pattern/halfpyramid.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class HalfPyramid{
+  public static void main(String args[]){
+    //t r=4;
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    System.out.println("------Half Pyramid---------\n You Selected Rows is "+r);
+    for (int i=1;i<=r;i++){
+      for (int j=1;j<=i;j++){
+      
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
+
+Output:
+
+------Half Pyramid---------
+ You Selected Rows is 5
+*
+**
+***
+****
+*****
+https://onecompiler.com/java/3yurfg894
